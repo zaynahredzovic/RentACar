8b69858aa6670dc6f9b71c09176ad0f74db79345
Zaynah Redzovic <zaynahredzovic@gmail.com>
2026-02-20 23:14:22 +0100

    added login

---
+++ app/Views/auth/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../public/css/style.css">
+    <title>Rent a car - login</title>
+</head>
+<body>
+    
+    <div class="container">
+        <h2>Login</h2>
+
+        <form id="loginForm">
+            <input type="email" name="email" placeholder="Email" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit">Login</button>
+        </form>
+
+        <p id="responseMsg"></p>
+        <p>No account? <a href="signup.php">Sign Up</a></p>
+
+    </div>
+
+    <script src="../../../public/js/jquery.min.js"></script>
+    <script src="../../../public/js/login.js"></script>
+</body>
+</html>
