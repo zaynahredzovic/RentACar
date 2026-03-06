cf158dd63451a7562d925759c870543a1a186c90
Zaynah Redzovic <zaynahredzovic@gmail.com>
2026-03-06 21:08:18 +0100

    fixed session class

--- app/core/Session.php
+++ app/core/Session.php
@@ -1,25 +1,29 @@
 <?php
-
 namespace App\Core; 
 
 class Session{
-
+    
     public static function init(){
         if(session_status() == PHP_SESSION_NONE){
             session_start();
         }
     }
 
-    public static function set($key,$value){
+    public static function set($key, $value){
+        self::init();
         $_SESSION[$key] = $value;
+        error_log("Session::set - {$key} = " . print_r($value, true));
     }
 
     public static function get($key){
-        return isset($_SESSION[$key]) ?? null;
+        self::init();
+        $value = isset($_SESSION[$key]) ? $_SESSION[$key] : null;
+        error_log("Session::get - {$key} = " . print_r($value, true));
+        return $value;
     }
 
     public static function destroy(){
+        self::init();
         session_destroy();
     }
-
 }
